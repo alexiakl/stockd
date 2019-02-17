3690a8fd85d384ff55586bf590b1b7de4214d932
Alexi Akl <alexiakl@gmail.com>
2019-02-17 15:06:20 +0200

    feat: add screen sizes

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import logo from '../static/images/logo/Stockd_1024.png';
 
 export default () => (
   <Navbar bg="light" expand="lg">
     <LinkContainer to="/">
-      <NavItem>stockd</NavItem>
+      <NavItem>
+        <img alt="logo" width="40" height="40" src={logo} />
+      </NavItem>
     </LinkContainer>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
         <LinkContainer to="/live">
-          <NavItem>Live</NavItem>
+          <NavItem>live</NavItem>
         </LinkContainer>
         <LinkContainer to="/historical">
-          <NavItem>Historical</NavItem>
+          <NavItem>historical</NavItem>
         </LinkContainer>
         <LinkContainer to="/performance">
-          <NavItem>Performance</NavItem>
+          <NavItem>performance</NavItem>
         </LinkContainer>
       </Nav>
       <LinkContainer to="/login">
